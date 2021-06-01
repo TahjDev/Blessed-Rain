cae938f48913ed93ec2f16ec306fcbde0681e3bd
tahj <tahjharris00@gmail.com>
2021-05-31 20:10:19 -0500

    setting up

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qc19wcm9qZWN0X3NrZWxldG9uLy4vc3JjL2luZGV4LmpzIl0sIm5hbWVzIjpbImNhbnZhcyIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJjdHgiLCJnZXRDb250ZXh0IiwieCIsIndpZHRoIiwieSIsImhlaWdodCIsImR4IiwiZHkiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsSUFBTUEsTUFBTSxHQUFHQyxRQUFRLENBQUNDLGNBQVQsQ0FBd0IsVUFBeEIsQ0FBZjtBQUNBLElBQU1DLEdBQUcsR0FBR0gsTUFBTSxDQUFDSSxVQUFQLENBQWtCLElBQWxCLENBQVo7QUFDQSxJQUFJQyxDQUFDLEdBQUdMLE1BQU0sQ0FBQ00sS0FBUCxHQUFlLENBQXZCO0FBQ0EsSUFBSUMsQ0FBQyxHQUFHUCxNQUFNLENBQUNRLE1BQVAsR0FBZ0IsRUFBeEI7QUFDQSxJQUFJQyxFQUFFLEdBQUcsQ0FBVDtBQUNBLElBQUlDLEVBQUUsR0FBRyxDQUFDLENBQVYsQyIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgY2FudmFzID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJteUNhbnZhc1wiKTtcbmNvbnN0IGN0eCA9IGNhbnZhcy5nZXRDb250ZXh0KFwiMmRcIik7XG5sZXQgeCA9IGNhbnZhcy53aWR0aCAvIDI7XG5sZXQgeSA9IGNhbnZhcy5oZWlnaHQgLSAzMDtcbmxldCBkeCA9IDI7XG5sZXQgZHkgPSAtMjtcbiJdLCJzb3VyY2VSb290IjoiIn0=
